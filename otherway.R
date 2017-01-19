463c555897f4d5c1aee5fe5235528d32b4dee8e9
LTaeHoon <lth1011@gmail.com>
2017-01-19 18:30:35 +0900

    수집 코드 수정

--- otherway.R
+++ otherway.R
@@ -1,5 +1,6 @@
 install.packages("rvest")
 library(rvest)
+library(data.table)
 
 #data 수집 과정 (data.go.kr에서 제공하는 API 사용)
 service_key <- "EeBjN2xdCzzcqHvefO0rZXaycAim0uGpKxnOX72PY1UpkSZnifzIK1kxLm61XXaQ4pFxhbW%2F%2FZbmQDKFiAFNVA%3D%3D"
@@ -7,43 +8,52 @@ service_key <- "EeBjN2xdCzzcqHvefO0rZXaycAim0uGpKxnOX72PY1UpkSZnifzIK1kxLm61XXaQ
 locCode <-c("11110","11140","11170","11200","11215","11230","11260","11290","11305","11320","11350","11380","11410","11440","11470","11500","11530","11545","11560","11590","11620","11650","11680","11710","11740")
 datelist <-c("201601","201602","201603","201604","201605","201606","201607","201608","201609","201610","201611","201612")
 
-
-url <- paste0("http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD=",locCode[1],"&DEAL_YMD=",datelist[1],"&serviceKey=",service_key)
+urllist <- list()
+cnt <-0
+for(i in 1:length(locCode)){
+  for(j in 1:length(datelist)){
+    cnt=cnt+1
+    urllist[cnt] <-paste0("http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD=",locCode[i],"&DEAL_YMD=",datelist[j],"&serviceKey=",service_key) 
+  }
+}
+#url <- paste0("http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD=",locCode[1],"&DEAL_YMD=",datelist[1],"&serviceKey=",service_key)
+apt_dt <- data.table()
+for(i in 1:length(urllist)){
 apt <- read_xml(url, encoding = 'utf-8')
 xml_structure(apt)
 items = xml_node(apt,'items')
 items
+#xml_node()에서 한글 사용을 위해 locale 값 변경 
 Sys.setlocale("LC_ALL", "C")
 #거래금액
 price = xml_text(xml_nodes(items,'거래금액'))
-price
 #건축년도
 con_year = xml_text(xml_nodes(items,"건축년도"))
 #년
 sale_year = xml_text(xml_nodes(items,"년"))
 #법정동
+dong = xml_text(xml_nodes(items,"법정동"))
 
 #아파트
-
+aptnm = xml_text(xml_nodes(items,"아파트"))
 #월
-
+month = xml_text(xml_nodes(items,"월"))
 #일
-
+day = xml_text(xml_nodes(items,"일"))
 #전용면적
+area = xml_text(xml_nodes(items,"전용면적"))
 
 #지번
+address = xml_text(xml_nodes(items,"지번"))
 
 #지역코드
+loc = xml_text(xml_nodes(items,"지역코드"))
 
 #층
+floor = xml_text(xml_nodes(items,"층"))
+Sys.setlocale("LC_ALL", "")
+apt_dt <- data.table(price,con_year,sale_year,dong,aptnm,month,day,area,address,loc,floor)
 
-iconv("嫄곕옒湲덉븸","utf-8","cp949")
-Sys.setlocale("LC_ALL", "ko_KR.UTF-8")
-Sys.getlocale()
-# "嫄곕옒湲덉븸"   "嫄댁텞\xeb뀈\xeb룄"               "\xeb뀈" 
-# 
-# "踰뺤젙\xeb룞"   "\xec븘\xed뙆\xed듃"               "\xec썡" 
-# 
-# "\xec씪"   "\xec쟾\xec슜硫댁쟻"       "吏\u0080踰\x88" 
-# 
-# "吏\u0080\xec뿭肄붾뱶"               "痢\xb5" 
+apt_dt<-rbind(apt_dt,apt)
+
+
