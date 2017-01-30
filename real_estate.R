c4742ce7a2523378f9e4dcfa37797df47e204579
taehoon <lth1011@gmail.com>
2017-01-31 00:07:39 +0900

    수집절차 수정코드 및 시계열 분석 추가

--- real_estate.R
+++ real_estate.R
@@ -10,8 +10,9 @@ service_key <- "EeBjN2xdCzzcqHvefO0rZXaycAim0uGpKxnOX72PY1UpkSZnifzIK1kxLm61XXaQ
 #서울시 지역코드
 locCode <-c("11110","11140","11170","11200","11215","11230","11260","11290","11305","11320","11350","11380","11410","11440","11470","11500","11530","11545","11560","11590","11620","11650","11680","11710","11740")
 locCode_nm <-c("종로구","중구","용산구","성동구","광진구","동대문구","중랑구","성북구","강북구","도봉구","노원구","은평구","서대문구","마포구","양천구","강서구","구로구","금천구","영등포구","동작구","관악구","서초구","강남구","송파구","강동구")
-datelist <-c("201601","201602","201603","201604","201605","201606","201607","201608","201609","201610","201611","201612")
-
+# datelist <-c("201501","201502","201503","201504","201505","201506","201507","201508","201509","201510","201511","201512","201401","201402","201403","201404","201405","201406","201407","201408","201409","201410","201411","201412","201601","201602","201603","201604","201605","201606","201607","201608","201609","201610","201611","201612")
+datelist <-c("201301","201302","201303","201304","201305","201306","201307","201308","201309","201310","201311","201312")
+# "201101","201102","201103","201104","201105","201106","201107","201108","201109","201110","201111","201112","201201","201202","201203","201204","201205","201206","201207","201208","201209","201210","201211","201212",
 
 #url <- paste0("http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD=11110&DEAL_YMD=201601&serviceKey=",service_key)
 
@@ -48,25 +49,26 @@ for(i in 1:length(urllist)){
                                 bungi=item_temp[9],
                                 loc=item_temp[10],
                                 floor=item_temp[11])
-     item[[i]]<-list(item_temp_dt)
-     #item<-rbind(item,t(as.data.table(item_temp)),fill=TRUE)
+     item[[i]]<-item_temp_dt
   }
   total[[i]]<-rbindlist(item)
 }
-result_apt_data <- rbindlist(total)
+result_apt_data2 <- rbindlist(total)
 #컬럼명
-names(result_apt_data)<-c("거래금액","건축년도","년","법정동","아파트","월","일","전용면적","지번","지역코드","층")
-save(result_apt_data, file="apt_item_sales_dt.Rdata")
+#names(result_apt_data)<-c("거래금액","건축년도","년","법정동","아파트","월","일","전용면적","지번","지역코드","층")
+save(result_apt_data2, file="apt_item_sales_dt2.Rdata")
 
 
 library(stringr)
 library(ggplot2)
-load("apt_item_sales_dt3.Rdata")
+load("apt_item_sales_dt.Rdata") #load 2014~2016년도 데이터
+load("apt_item_sales_dt2.Rdata") #load 2013년도 데이터
 
-apt_data<-data.table(item)
+apt_data1<-data.table(result_apt_data)
+apt_data2<-data.table(result_apt_data2)
 #apt_data<-data.table(result_apt_data)
-colnames(apt_data) <- c('price','con_year','year','dong','aptnm','month','dat','area','bungi','loc','floor')
-
+#colnames(apt_data) <- c('price','con_year','year','dong','aptnm','month','dat','area','bungi','loc','floor')
+apt_data<-rbindlist(list(apt_data1,apt_data2))
 
 apt_data[,price:=as.character(price)%>%str_trim()%>%sub(",","",.)%>%as.numeric()]
 apt_data[,con_year:=as.character(con_year)%>%str_trim()%>%as.numeric()]
@@ -79,17 +81,16 @@ apt_data[,dong:=as.character(dong)%>%str_trim()]
 apt_data[,yyyyqrt:=paste0(year,qrt)]
 apt_data[,month:=as.character(month)%>%str_trim()%>%as.numeric()]
 apt_data[,yyyym:=paste0(year,month)]
-lapply(locCode,function(x){apt_data[loc==x,gu:=locCode_nm[which(locCode==x)]]})
-
-summary(apt_data)
-str(apt_data)
-head(apt_data)
-#번지가 없는 데이터에 대한 처리
+#loc가 잘못 들어가 있는 데이터에 대한 처리
 index_na <-is.na(apt_data$floor)
 apt_data[index_na]$floor <- apt_data[index_na]$loc
 apt_data[index_na]$loc <- apt_data[index_na]$bungi
 apt_data[index_na]$bungi <- NA
-apt_data[index_na]$floor
+
+lapply(locCode,function(x){apt_data[loc==x,gu:=locCode_nm[which(locCode==x)]]})
+# 데이터 확인
+unique(apt_data$loc)
+unique(apt_data$gu)
 
 #서울 지역 아파트 매매가격 추이, 매매가격 예측이 가능한가?
 
@@ -104,7 +105,7 @@ ggplot(apt_data,aes(x=yyyyqrt,y=price))+
 
 # 서울시 구/분기별 아파트 매매 가격
 
-apt_data_by_gu_qrt <- aggregate(apt_data$price, apt_data[,c("yyyyqrt","gu")], mean)
+apt_data_by_gu_qrt <- aggregate(apt_data$price, by=list(apt_data$yyyyqrt,apt_data$gu), mean)
 names(apt_data_by_gu_qrt) <- c('yyyyqrt','gu','pricemean')
 
 ggplot(apt_data_by_gu_qrt,aes(x=yyyyqrt,y=pricemean,group=gu))+
@@ -113,48 +114,13 @@ ggplot(apt_data_by_gu_qrt,aes(x=yyyyqrt,y=pricemean,group=gu))+
   stat_smooth(method="lm")
 
 
-#특정구 동별 평균 매매가 추이
-# 일단 구별(loc)로 나눈 구별 테이블이 필요
-dt<-data.table()
-for(i in 1:length(locCode)){
-  dt[[i]] <- subset(apt_data,loc==locCode[[i]])
-}
-
-# 동별 분기별 평균 매매가
-data1<-as.data.table(dt[[1]])
-data2<-as.data.table(dt[[2]])
-data3<-as.data.table(dt[[3]])
-data4<-as.data.table(dt[[4]])
-data5<-as.data.table(dt[[5]])
-data6<-as.data.table(dt[[6]])
-data7<-as.data.table(dt[[7]])
-data8<-as.data.table(dt[[8]])
-data9<-as.data.table(dt[[9]])
-data10<-as.data.table(dt[[10]])
-data11<-as.data.table(dt[[11]])
-data12<-as.data.table(dt[[12]])
-data13<-as.data.table(dt[[13]])
-data14<-as.data.table(dt[[14]])
-data15<-as.data.table(dt[[15]])
-data16<-as.data.table(dt[[16]])
-data17<-as.data.table(dt[[17]])
-data18<-as.data.table(dt[[18]])
-data19<-as.data.table(dt[[19]])
-data20<-as.data.table(dt[[20]])
-data21<-as.data.table(dt[[21]])
-data22<-as.data.table(dt[[22]])
-data23<-as.data.table(dt[[23]])
-data24<-as.data.table(dt[[24]])
-data25<-as.data.table(dt[[25]])
-
-data1_by_dong_qrt<-aggregate(data1$price,by=list(data1$yyyym,data1$dong),mean)
 
 #시계열 분석
 #시계열에 패턴이 존재한다면 미래에도 이러한 패턴이 계속 될 것이라는 가정하에 예측을 해볼 수 있다.
 # 그렇다면 시계열에 패턴이 존재하는지 확인하는 방법은 시계열이 랜덤한지를 알아보는 랜덤성 검증이 있다.
 # 랜덤성 검정 -> 런검정 
 #
-gu_meanprice <- aggregate(apt_data$price,apt_data[,c('yyyym','gu')],mean)
+gu_meanprice <- aggregate(apt_data$price,by=list(apt_data$yyyym,apt_data$gu),mean)
 head(gu_meanprice)
 names(gu_meanprice)<- c('yyyym','gu','price')
 #중복없이 구 추출
@@ -172,14 +138,16 @@ ggplot(data.table(gu_list, runs_p), aes(x=gu_list, y=runs_p, group=1)) +
   geom_line() + geom_point() +
   ylab('P-value') + xlab('구')
 
-#도봉구를 제외하고 p값이 0.05 보다 작으므로 귀무가설 (랜덤하다)기각 할 수 있다 
+which(runs_p>0.05)
+
+#모든 구에 대해서 p값이 0.05 보다 작으므로 귀무가설 (랜덤하다)기각 할 수 있다 
 #따라서 랜덤하지 않은 어떤 패턴이 있다고 할 수 있다.
 
 # 그럼 이중에서 내가 살고 있는 관악구를 추출하여 시계열 패턴을 그려보겠다.
 gwanak_data <- apt_data[gu=="관악구",]
-gwanak_price <- aggregate(gwanak_data$price,gwanak_data[,'yyyym'],mean) 
-names(gwanak_price)<-c('yyyym','price')
-tot_ts <- ts(gwanak_price$price,start=c(2006,1),frequency = 12)
+gwanak_price <- aggregate(gwanak_data$price,by=list(gwanak_data$yyyyqrt),mean) 
+names(gwanak_price)<-c('yyyyqrt','price')
+tot_ts <- ts(gwanak_price$price,start=c(2013,1),frequency = 4)
 plot(tot_ts)
 
 install.packages("forecast")
